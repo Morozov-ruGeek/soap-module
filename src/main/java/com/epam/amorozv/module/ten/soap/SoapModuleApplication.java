ff4176843e18c1b333774a7ce4fd8124d4124443
Aleksei Morozov <aleksei_morozov@epam.com>
2021-12-13 07:18:39 +0400

    Initial commit

---
+++ src/main/java/com/epam/amorozv/module/ten/soap/SoapModuleApplication.java
@@ -0,0 +1,13 @@
+package com.epam.amorozv.module.ten.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoapModuleApplication.class, args);
+    }
+
+}
